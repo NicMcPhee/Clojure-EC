7e0f63f73aa37a3252fae7b3fede9ce98aa16a62
Nic McPhee <mcphee@morris.umn.edu>
2010-10-03 17:58:45 -0500

    Did some performance tweaks that improved the performance somewhat by converting lazy sequences to vectors with (vec ...).

--- src/ec/core.clj
+++ src/ec/core.clj
@@ -1,3 +1,7 @@
+;;; TO-DOs
+;;; Improve performance of tournament-selection (which profiling shows really sucks).
+;;; Look at parallelizing the creation of new generations so we can use multi-cores.
+
 (ns ec.core)
 
 (defn- choices-seq
@@ -21,8 +25,13 @@
    The resulting child will take its first j elements from first-parent,
    elements j<=i<k from the second parent, and elements k<=i from
    the first parent again."
-  [first-parent second-parent j k]
-  (masked-xo (choices-seq j k) first-parent second-parent))
+  ([first-parent second-parent]
+    (let [length (count first-parent)
+         first-point (rand-int length)
+         second-point (rand-int length)]
+      (two-point-xo first-parent second-parent first-point second-point)))
+  ([first-parent second-parent j k]
+    (masked-xo (choices-seq j k) first-parent second-parent)))
 
 (defn masked-point-mutation
   "Use the given mask to perform point mutation on a sequence. Where
@@ -43,9 +52,11 @@
 
 (defn point-mutation
   "Randomly flip the bits in the given sequence with uniform probability."
-  [parent]
-  (let [random-bits (repeatedly #(rand-int 2))]
-    (masked-point-mutation parent random-bits)))
+  ([parent mutation-rate]
+   (let [random-bits (repeatedly #(if (< (rand) mutation-rate) 1 0))]
+    (masked-point-mutation parent random-bits)) )
+  ([parent]
+    (point-mutation parent (/ 1.0 (count parent)))))
 
 (defn tournament-selection
   "Generates a tournament with no duplication having the specified size and
@@ -62,7 +73,7 @@
   (let 
     [random-bits (repeatedly num-bits #(rand-int 2))
      fitness (fitness-function random-bits)]
-    {:bit-string random-bits
+    {:bit-string (vec random-bits)
      :fitness fitness}))
 
 (defn make-population
@@ -70,3 +81,30 @@
    and using the given fitness function."
   [num-individuals num-bits fitness-function]
   (repeatedly num-individuals #(make-individual num-bits fitness-function)))
+
+(defn child
+  [population fitness-function tournament-size]
+  (let [first-parent (:bit-string (tournament-selection population tournament-size))
+        second-parent (:bit-string (tournament-selection population tournament-size))
+        xo-result (two-point-xo first-parent second-parent)
+        mutation-result (point-mutation xo-result)
+        result {:bit-string (vec mutation-result) :fitness (fitness-function mutation-result)}]
+    result))
+
+(defn next-generation
+  [num-individuals fitness-function tournament-size population]
+  (map (fn [_] (child population fitness-function tournament-size)) (range num-individuals)))
+
+(defn count-ones
+  [bit-string]
+  (apply + bit-string))
+
+(defn run-ga
+  [num-gens num-individuals num-bits fitness-function tournament-size]
+  (let [initial-pop (make-population num-individuals num-bits fitness-function)]
+    (take num-gens (iterate #(next-generation num-individuals fitness-function tournament-size %1) initial-pop))))
+
+(defn do-run-ga
+  []
+  (let [pops (run-ga 100 1000 50 count-ones 2)]
+    (map #(apply max (map :fitness %)) pops)))
